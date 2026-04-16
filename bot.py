2b3641ab0b29bea9e930b6587bf9d3a8dec5f2f7
ANI_OTAKU <dhirenyadav2007@gmail.com>
2026-04-16 19:47:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@
 import os
 from aiohttp import web
 from plugins import web_server
-from threading import Thread
-from flask import Flask
+#from threading import Thread
+#from flask import Flask
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
@@ -18,20 +18,20 @@ version = "v1.0.0"
 # ✅ FLASK + THREAD (Render Support)
 # ──────────────────────────────
 
-flask_app = Flask(__name__)
+#flask_app = Flask(__name__)
 
-@flask_app.route("/")
-def home():
-    return "Bot is running!", 200
+#@flask_app.route("/")
+#def home():
+#    return "Bot is running!", 200
 
 
-def run_flask():
-    flask_app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
-        threaded=True,
-        use_reloader=False
-    )
+#def run_flask():
+#    flask_app.run(
+#        host="0.0.0.0",
+#        port=int(os.environ.get("PORT", 10000)),
+#        threaded=True,
+#        use_reloader=False
+#    )
 
 #================================================
 
